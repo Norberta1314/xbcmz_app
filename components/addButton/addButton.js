1a57470f4680abff65ea09e8ee648d136d736650
zhutianyu <704735206@qq.com>
2019-02-27 17:11:43 +0800

    fix library height apace at the iponeX's trouble

--- components/addButton/addButton.js
+++ components/addButton/addButton.js
@@ -8,12 +8,60 @@ Component({
     data: {
         x: 0,
         y: 0,
+        isMove: false,
+        menuShow: false,
+        initalTouchX: 0,
+        initalTouchY: 0,
+        currentTouchX: 0,
+        currentTouchY: 0,
+        transform: ''
+
     },
     methods: {
         changeHandle(e) {
             console.log(app.systemInfo)
             console.log(this.data.x, this.data.y)
+        },
+        touchStartHandle(e) {
+
+
+        },
+        touchMoveHandle(e) {
+            console.log(e)
+            if (this.data.isMove) {
+                const tempTransform =  `translateX(${e.touches[0].pageX - this.data.initalTouchX}px) translateY(${e.touches[0].pageY - this.data.initalTouchY}px)`
+                this.setData({
+                    transform: tempTransform
+                })
+            }
+
+        },
+        touchEndHandle(e) {
+            // this.setData({
+            //     isMove: false
+            // })
+        },
+        tapHandle(e) {
+            console.log(e)
+            this.setData({
+                menuShow: !this.data.menuShow
+            })
+
+        },
+        longPressHandle(e) {
+            console.log(e)
+            if (this.data.isMove) {
+                return
+            }
+            this.setData({
+                isMove: true,
+                initalTouchX: e.touches[0].pageX,
+                initalTouchY: e.touches[0].pageY,
+            })
+
+
         }
+
     },
     pageLifetimes: {
         show() {
