bad9535e30bd1b67e434b3230e988497ebe8ef1b
norberta <norbertahp@163.com>
2019-02-28 15:45:13 +0800

    add_button style grade&testTable

--- pages/learning/subPage/testTable/index.js
+++ pages/learning/subPage/testTable/index.js
@@ -1,5 +1,26 @@
 const app = getApp();
 
 Page({
-  
+  data: {
+    tests:[{
+      name: '自动控制原理',
+      time: '2019年6月10日 8：10',
+      place: '广之A401',
+      seat: '1',
+      day: 30
+    }, {
+      name: '自动控制原理',
+      time: '2019年6月10日 8：10',
+      place: '广之A401',
+      seat: '1',
+      day: 30
+    }, {
+      name: '自动控制原理与自动控制原理方案',
+      time: '2019年6月10日 8：10',
+      place: '广之A401',
+      seat: '1',
+      day: 30
+    }]
+  }
+
 })
