8e2496a3b5c7e7df50d47765a4be3f2a57a06413
norberta <norbertahp@163.com>
2019-02-28 17:44:33 +0800

    课程资料和作业管理

--- pages/learning/subPage/materials/index.js
+++ pages/learning/subPage/materials/index.js
@@ -1,5 +1,18 @@
 const app = getApp();
 
 Page({
-  
+  data:{
+    courses:[
+      {name:'数据结构'},
+      {name:'离散数学'},
+      {name:'高等数学'},
+      {name:'概率论'},
+      {name:'角色造型'},
+      {name:'数据库'},
+      {name:'操作系统'},
+      {name:'计算机原理'},
+      {name:'计算机网络'}
+    ]
+  }
+
 })
