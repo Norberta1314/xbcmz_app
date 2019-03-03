1193d96c0006cf60c691f8977c4a4fcfe7ccc8fd
norberta <norbertahp@163.com>
2019-03-03 23:11:57 +0800

    搜索书籍以及书籍详情

--- pages/library/subPage/search/index.js
+++ pages/library/subPage/search/index.js
@@ -9,5 +9,11 @@ Page({
     this.setData({
       name:val
     })
+  },
+  bindTapSearch: function (e) {
+    var name = this.data.name
+    wx.navigateTo({
+      url: `/pages/library/subPage/search/subPage/index?name=${name}`,
+    })
   }
 })
