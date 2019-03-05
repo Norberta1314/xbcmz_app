ad0f2959b35b72f748d807da88c9304224ded7d5
norberta <norbertahp@163.com>
2019-03-05 14:00:57 +0800

    教学资料详情页

---
+++ pages/learning/subPage/materials/subPage/index.js
@@ -0,0 +1,74 @@
+// pages/learning/subPage/task/subPage/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    listData:[
+      {"text":"作业一"},
+      {"text":"作业二"},
+      {"text":"作业三"},
+      {"text":"作业四"},
+      {"text":"作业五"},
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log('onLoad')
+  },
+
+  /**
+   * 生命周期
+   * 函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
