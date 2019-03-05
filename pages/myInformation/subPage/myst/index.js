bcf67a0db0a194c663d6f73c19b03837256915bc
norberta <norbertahp@163.com>
2019-03-05 20:56:13 +0800

    myst

--- pages/myInformation/subPage/myst/index.js
+++ pages/myInformation/subPage/myst/index.js
@@ -1,5 +1,14 @@
 const app = getApp();
 
 Page({
-  
+  bindHeidong:function (e) {
+    wx.navigateTo({
+      url: '/pages/myInformation/subPage/myst/subPage/heidong/index'
+    })
+  },
+  bindBilinxing:function (e) {
+    wx.navigateTo({
+      url: '/pages/myInformation/subPage/myst/subPage/bilinxing/index'
+    })
+  },
 })
