77d601d841153f0bf00d01a9dd475ce0ca467ed7
norberta <norbertahp@163.com>
2019-03-06 19:04:56 +0800

    myst

--- pages/myInformation/subPage/myst/subPage/bilinxing/index.js
+++ pages/myInformation/subPage/myst/subPage/bilinxing/index.js
@@ -1,5 +1,23 @@
 const app = getApp();
 
 Page({
-
+  data:{
+    friend:{
+      name:'zxz',
+      info:'热爱学习热爱生活，想找女朋友'
+    },
+    showFriend:0,
+  },
+  tapShowFriend: function (e) {
+    console.log("1")
+    this.setData({
+      showFriend: 1,
+    })
+  },
+  tapUnshowFriend: function (e) {
+    console.log("23")
+    this.setData({
+      showFriend: 0,
+    })
+  }
 })
