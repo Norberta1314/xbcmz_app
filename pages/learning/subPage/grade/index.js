a17d6eff8b671b0701ab1877d3591cd70b861413
norberta <norbertahp@163.com>
2019-03-17 22:54:15 +0800

    成绩查询前后端交互以及考试安排banner

--- pages/learning/subPage/grade/index.js
+++ pages/learning/subPage/grade/index.js
@@ -6,44 +6,44 @@ Page({
     sID: '201600001111',
     select: false,
     term: '2017/2018(1)',
-    gpa: 4.15,
-    scores: [{
-      score: 98,
-      name: '自动控制原理'
-    }, {
-      score: 90,
-      name: '商务英语'
-    }, {
-      score: 93,
-      name: '工程经济'
-    }, {
-      score: 85,
-      name: '角色造型'
-    }, {
-      score: 90,
-      name: '操作系统原理'
-    }, {
-      score: 90,
-      name: '计算机动画'
-    }, {
-      score: 98,
-      name: '自动控制原理'
-    }, {
-      score: 90,
-      name: '商务英语'
-    }, {
-      score: 93,
-      name: '工程经济'
-    }, {
-      score: 85,
-      name: '角色造型'
-    }, {
-      score: 90,
-      name: '操作系统原理'
-    }, {
-      score: 90,
-      name: '计算机动画'
-    }]
+    gpa: 0.00,
+    scores: ''
+  },
+  onLoad: function () {
+    let _this = this
+    app.$store.setWxCtx(_this, 'grade')
+    _this.setName()
+    _this.getGrade()
+  },
+  setName() {
+    let _this = this
+    app.fetch({
+      url: app.API('myInfo'),
+      method: 'GET',
+      success: (res) => {
+        const result = res.data.data
+        _this.setData({
+          name: result.name
+        })
+      }
+    })
+    this.setState({
+      sID:app.$store.getAppState('username')
+    })
+
+  },
+  getGrade() {
+    let _this = this
+    app.fetch({
+      url: app.API('grade'),
+      method: 'GET',
+      success: (res) => {
+        const result = res.data.data
+        this.setState({
+          scores: result
+        })
+      }
+    })
   },
   bindShowMsg: function () {
     this.setData({
